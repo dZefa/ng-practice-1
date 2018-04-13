787b81d4921de28c26c7fe90c7136a2518859ef6
Daniel Chong <danchong625@gmail.com>
2018-04-13 01:20:53 -0700

    [feat] removes routes and adds templates

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
 
-// import Components from './components/components';
-
-import { provideStates } from '../util/util';
-import { routes } from './app.route';
+import components from './components/components';
+import template from './app.html';
 
 let myApp = angular.module('app', [
   uiRouter,
+  components
 ]);
 
-myApp.config(($stateProvider, $urlServiceProvider) => {
-  provideStates(routes, $stateProvider);
-  $urlServiceProvider.otherwise('/');
+myApp.config(($locationProvider) => {
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false,
+  });
+})
+.component('app', {
+  template
 });
